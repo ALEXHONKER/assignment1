fd77f39fec0bb243888cd39a2ca4d592c93a5d6b
chenyang@mac <alexhonker@gmail.com>
2015-10-06 02:20:07 -0400

    3rd with full

--- assign1.py
+++ assign1.py
@@ -1,14 +1,18 @@
 import string
 import sys
-
+import math
+global count
 dic={}
 
+nodes={}
+edges=[]
 class street:
 	def __init__(self):
 		self.name=""
 		self.setxy=[]
 		self.segline=[]
 		self.intersec=[]
+		self.no={}
 	def setsxy(self,temp):
 		self.setxy.append(temp)
 	def setname(self,na):
@@ -98,6 +102,9 @@ def graph():
 			x1=coor[i][0]+0.0
 			y1=coor[i][1]+0.0
 			if i != (len(coor)-1):
+				newstr=dic[strs]
+				newstr.no={}
+				dic[strs]=newstr
 				x2=coor[i+1][0]+0.0
 				y2=coor[i+1][1]+0.0
 				print "x"
@@ -114,8 +121,8 @@ def graph():
 					for strs2 in str_name2:
 						coor2=dic[strs2].setxy
 						for j in range(len(coor2)):
-							x3=coor2[j][0]+0.0
-							y3=coor2[j][1]+0.0
+							x3=coor2[j][0]
+							y3=coor2[j][1]
 							if j != (len(coor2)-1):
 								x4=coor2[j+1][0]+0.0
 								y4=coor2[j+1][1]+0.0
@@ -131,8 +138,82 @@ def graph():
 									yy=k2*xx+b2
 									if  yy>=miny and yy<=maxy:
 										new_street=dic[strs]
-										new_street.addintersec([xx,yy])
+										new_street.addintersec([xx,yy])	
+
+										nidx1y1=str(x1)+'_'+str(y1)
+										if not nodes.has_key(nidx1y1):
+											pairx1y1=[[x1,y1],count]
+											count=count+1
+											nodes[nidx1y1]=pairx1y1
+											new_street.no[nidx1y1]=pairx1y1
+										else:
+											if not new_street.no.has_key(nidx1y1):
+												pairx1y1=nodes[nidx1y1]
+												new_street.no[nidx1y1]=pairx1y1
+
+
+										nidx2y2=str(x2)+'_'+str(y2)
+										if not nodes.has_key(nidx2y2):
+											pairx2y2=[[x2,y2],count]
+											count=count+1
+											nodes[nidx2y2]=pairx2y2
+											new_street.no[nidx2y2]=pairx2y2
+										else:
+											if not new_street.no.has_key(nidx2y2):
+												pairx2y2=nodes[nidx2y2]
+												new_street.no[nidx2y2]=pairx2y2
+
+										nid=str(xx)+'_'+str(yy)										
+										if not nodes.has_key(nid):
+											pair=[[xx,yy],count]
+											count=count+1
+											nodes[nid]=pair
+											new_street.no[nid]=pair
+										else:
+											if not new_street.no.has_key(nid):
+												pair2=nodes[nid]
+												new_street.no[nid]=pair2
+
+
 										dic[strs]=new_street    # add the intersection node
+
+					my=y1
+					ny=y2
+					nidx1y1=str(x1)+'_'+str(y1)
+					nidx2y2=str(x2)+'_'+str(y2)
+					idmin=nidx2y2
+					idmax=nidx1y1
+					if y2>y1:
+						my=y2
+						ny=y1
+						idmin=nidx1y1
+						idmax=nidx2y2
+					list_node=dic[strs].no.keys()
+					if not list_node==[]:
+						list_node.remove(nidx1y1)
+						list_node.remove(nidx2y2)
+						if not list_node==[]:
+							list_node.append(idmax)	
+							mindis=9999999.0
+							minpair=[]
+							minid2=idmin
+							while list_node!=[]:
+								for ns in list_node:
+									pair3=dic[strs].no[ns]
+									dis=pair3[0][1]-ny
+									if dis<mindis:
+										mindis=dis
+										minpair=pair3
+										minid2=ns
+								edges.append([dic[strs].no[idmin][1],minpair[1]])
+								idmin=minid2
+								ny=minpair[0][1]
+								list_node.remove(minid2)
+
+
+
+					
+
 				else:
 					k1=(y2-y1)/(x2-x1)
 					b1=y2-k1*x2
@@ -156,8 +237,45 @@ def graph():
 									yy=k1*xx+b1
 									if  yy>=miny and yy<=maxy:
 										new_street=dic[strs]
-										new_street.addintersec([xx,yy])
+										new_street.addintersec([xx,yy])	
+
+										nidx1y1=str(x1)+'_'+str(y1)
+										if not nodes.has_key(nidx1y1):
+											pairx1y1=[[x1,y1],count]
+											count=count+1
+											nodes[nidx1y1]=pairx1y1
+											new_street.no[nidx1y1]=pairx1y1
+										else:
+											if not new_street.no.has_key(nidx1y1):
+												pairx1y1=nodes[nidx1y1]
+												new_street.no[nidx1y1]=pairx1y1
+
+
+										nidx2y2=str(x2)+'_'+str(y2)
+										if not nodes.has_key(nidx2y2):
+											pairx2y2=[[x2,y2],count]
+											count=count+1
+											nodes[nidx2y2]=pairx2y2
+											new_street.no[nidx2y2]=pairx2y2
+										else:
+											if not new_street.no.has_key(nidx2y2):
+												pairx2y2=nodes[nidx2y2]
+												new_street.no[nidx2y2]=pairx2y2
+
+										nid=str(xx)+'_'+str(yy)										
+										if not nodes.has_key(nid):
+											pair=[[xx,yy],count]
+											count=count+1
+											nodes[nid]=pair
+											new_street.no[nid]=pair
+										else:
+											if not new_street.no.has_key(nid):
+												pair2=nodes[nid]
+												new_street.no[nid]=pair2
+
+
 										dic[strs]=new_street    # add the intersection node
+
 								else:
 									k2=(y4-y3)/(x4-x3)
 									b2=y4-k2*x4
@@ -176,12 +294,75 @@ def graph():
 										yy=k1*xx+b1
 										if xx>=minx and xx<=maxx and yy>=miny and yy<=maxy:
 											new_street=dic[strs]
-											new_street.addintersec([xx,yy])
+											new_street.addintersec([xx,yy])	
+
+											nidx1y1=str(x1)+'_'+str(y1)
+											if not nodes.has_key(nidx1y1):
+												pairx1y1=[[x1,y1],count]
+												count=count+1
+												nodes[nidx1y1]=pairx1y1
+												new_street.no[nidx1y1]=pairx1y1
+											else:
+												if not new_street.no.has_key(nidx1y1):
+													pairx1y1=nodes[nidx1y1]
+													new_street.no[nidx1y1]=pairx1y1
+
+
+											nidx2y2=str(x2)+'_'+str(y2)
+											if not nodes.has_key(nidx2y2):
+												pairx2y2=[[x2,y2],count]
+												count=count+1
+												nodes[nidx2y2]=pairx2y2
+												new_street.no[nidx2y2]=pairx2y2
+											else:
+												if not new_street.no.has_key(nidx2y2):
+													pairx2y2=nodes[nidx2y2]
+													new_street.no[nidx2y2]=pairx2y2
+
+											nid=str(xx)+'_'+str(yy)										
+											if not nodes.has_key(nid):
+												pair=[[xx,yy],count]
+												count=count+1
+												nodes[nid]=pair
+												new_street.no[nid]=pair
+											else:
+												if not new_street.no.has_key(nid):
+													pair2=nodes[nid]
+													new_street.no[nid]=pair2
+
+
 											dic[strs]=new_street    # add the intersection node
+					mx1=x1
+					my1=y1
+					nidx1y1=str(x1)+'_'+str(y1)
+					nidx2y2=str(x2)+'_'+str(y2)
+					idmin=nidx1y1
+					idmax=nidx2y2
+					list_node=dic[strs].no.keys()
+					if not list_node==[]:
+						list_node.remove(nidx1y1)
+						list_node.remove(nidx2y2)
+						if not list_node==[]:
+							list_node.append(idmax)	
+							mindis=9999999.0
+							minpair=[]
+							minid2=idmin
+							while list_node!=[]:
+								for ns in list_node:
+									pair3=dic[strs].no[ns]
+									dis=math.sqrt((pair3[0][1]-my1)**2+(pair3[0][0]-mx1)**2)
+									if dis<mindis:
+										mindis=dis
+										minpair=pair3
+										minid2=ns
+								edges.append([dic[strs].no[idmin][1],minpair[1]])
+								idmin=minid2
+								ny=minpair[0][1]
+								list_node.remove(minid2)
 
 			
 
-
+count=0
 while True:
 	stdin=sys.stdin.readlines()
 	if not stdin:
